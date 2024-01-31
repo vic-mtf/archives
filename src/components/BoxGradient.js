4dc0db2b7f980ec5c4e01649988208707886e334
vic-mtf <phalphie1@gmail.com>
2024-01-31 10:47:35 +0100

    update

--- src/components/BoxGradient.js
+++ src/components/BoxGradient.js
@@ -21,3 +21,5 @@ export default BoxGradient;
 BoxGradient.defaultProps = {
   colors: ['#0095c940', '#fff24b40', '#db383240'],
 }
+
+//background-image: linear-gradient(0deg, #0095c940, transparent 70.71%), linear-gradient(120deg, #fff24b40, transparent 70.71%), linear-gradient(240deg, #db383240, transparent 70.71%);
