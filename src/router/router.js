e76d5dfdeb136c9a7a55f845373b25ffbf1389c5
Mboob <91790923+vic-mtf@users.noreply.github.com>
2025-01-16 18:31:17 +0100

    redirect baseurl

--- src/router/router.js
+++ src/router/router.js
@@ -3,20 +3,22 @@ import { createBrowserRouter } from "react-router-dom";
 import AppTest from "../test/App.test";
 import Archives from "../views/Archives";
 
-const router = createBrowserRouter(
-  [
-    {
-      element: AppTest,
-      path: "/test",
-    },
-    {
-      element: Archives,
-      path: "*",
-    },
-  ].map(({ element, ...data }) => ({
-    element: React.createElement(element),
-    ...data,
-  }))
-);
+const PUBLIC_URL = import.meta.env.BASE_URL;
+
+const routes = [
+  {
+    element: AppTest,
+    path: "/test",
+  },
+  {
+    element: Archives,
+    path: "*",
+  },
+].map(({ element, ...data }) => ({
+  element: React.createElement(element),
+  ...data,
+}));
+
+const router = createBrowserRouter(routes, { basename: PUBLIC_URL });
 
 export default router;
